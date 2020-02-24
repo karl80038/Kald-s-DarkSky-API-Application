1c9a8b24abe5143c15b96d098f89515aaa44d8ce
Karl <karl80038@gmail.com>
2020-02-24 22:01:01 +0200

    Added a temporary API key; The key will later be disabled.

--- darksky.js
+++ darksky.js
@@ -6,7 +6,7 @@ class Darksky
         this.latitude = "59.437222";
         this.longitude = "24.745278";
         this.api_address = "https://api.darksky.net/forecast/";
-        this.api_key = "-PASTE YOUR API KEY HERE-";
+        this.api_key = "c17ddb7db5d237be2ffcce6db22480d7";
         this.proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     }
 
